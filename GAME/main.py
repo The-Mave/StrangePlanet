035d582ff93d92f9e5aff77c31ac8cf03bdc7da6
TheMave <78883240+The-Mave@users.noreply.github.com>
2021-10-14 20:58:48 -0300

    Add files via upload

---
+++ GAME/main.py
@@ -0,0 +1,30 @@
+import pygame
+pygame.init()
+
+# DEVELOPER MODE
+DEV_MODE = 1
+
+
+WIDTH = 1024
+HEIGHT = 768
+TILESIZE = 32
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
+
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+pygame.display.update()
+pygame.quit()
+
+
+
+
+
+
